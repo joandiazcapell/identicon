8e74e43331894273f29418991349ce1057c1a7f9
Joan Diaz <jdiaz@newrelic.com>
2024-02-04 20:00:19 +0100

    Remove odd squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,8 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> build_grid()
+    |> filter_odd_squares()
   end
 
   @doc """
@@ -36,6 +38,32 @@ defmodule Identicon do
   """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex } = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3,3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    # [145, 46, 200]
+
+    # [145, 46, 200, 46, 145]
+    row ++ [second,first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
 end
