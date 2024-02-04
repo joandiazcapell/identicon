2ea1efc2c1700d35f2c70272a6ec53764c4b5a17
Joan Diaz <jdiaz@newrelic.com>
2024-02-04 18:53:05 +0100

    Adding a struct to store the data and the function pick_color with its documentation

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,43 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for `Identicon`.
+    Creates an identicon images based on a string.
   """
+  def main(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+  end
 
   @doc """
-  Hello world.
+    Returns a list of 16 integers based on a MD5 from a string.
 
-  ## Examples
+    ## Examples
 
-      iex> Identicon.hello()
-      :world
+      iex> Identicon.hash_input("banana")
+      %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
 
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
+
+  @doc """
+    Returns the r,g,b numbers for the identicon's color
+
+    ## Examples
+
+      iex> image = Identicon.hash_input("banana")
+      iex> Identicon.pick_color(image)
+      [114, 179, 2]
+
+  """
+  def pick_color(image) do
+    %Identicon.Image{hex: [r,g, b | _tail]} = image
+
+    [r,g,b]
+  end
+
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,9 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "List of integers coming from a string into a MD5 coding" do
+
+    %{hex: hex} = Identicon.hash_input("banana")
+    assert length(hex) == 16
   end
 end
